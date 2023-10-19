71e1d6f4ecec917493f1254370ce24c06ae2efc3
Ganesh Bikshandi <ganesh@colfax-intl.com>
2023-10-19 03:53:37 +0000

    added README.

--- include/utils/cuda_launch.hpp
+++ include/utils/cuda_launch.hpp
@@ -1,20 +1,16 @@
-namespace cfk
-{
-namespace utils
-{
-void set_smem_size(int smem_size, void const* kernel)
-{
-        // account for dynamic smem capacity if needed
-    if (smem_size >= (48 << 10)) {
-      cudaError_t result = cudaFuncSetAttribute(
-          kernel,
-          cudaFuncAttributeMaxDynamicSharedMemorySize,
-          smem_size);
-      if (cudaSuccess != result) {
-        result = cudaGetLastError(); // to clear the error bit
-        std::cout << "  cudaFuncSetAttribute() returned error: " << cudaGetErrorString(result);
-      }
+namespace cfk {
+namespace utils {
+void set_smem_size(int smem_size, void const *kernel) {
+  // account for dynamic smem capacity if needed
+  if (smem_size >= (48 << 10)) {
+    cudaError_t result = cudaFuncSetAttribute(
+        kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
+    if (cudaSuccess != result) {
+      result = cudaGetLastError(); // to clear the error bit
+      std::cout << "  cudaFuncSetAttribute() returned error: "
+                << cudaGetErrorString(result);
     }
+  }
 }
-}
-}
+} // namespace utils
+} // namespace cfk
